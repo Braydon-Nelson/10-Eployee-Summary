2e6295c65955c139f5bf66fe7c998a0f3b46b5ac
Braydon Nelson <braydon.nelson@outlook.com>
2020-02-01 12:44:39 -0700

    added required links

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const http = require('http');
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+//create filler items for input to go into
+
+//prompt user for info
+
+//create html based on input
