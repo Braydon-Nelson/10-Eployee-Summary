55fb601f1cca5008605909b9686970dd3770690f
Braydon Nelson <braydon.nelson@outlook.com>
2020-02-06 23:22:57 -0700

    fully functional and styled

--- templates/html.js
+++ templates/html.js
@@ -14,14 +14,16 @@ const generateHTML = function (teamstr) {
 
 <body>
 
+<div class="container">
+
 <div>
    <h1>My Team</h1>
 </div>
-<div>
+<div class="row">
 ${teamstr} 
 </div>
 
-
+</div>
 </body>
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
@@ -42,21 +44,26 @@ const generateCard = function (arr) {
         roleInfo = `School: ${arr.school}`
     }
 
-    return `<div class="card">
-<div class="card-header">
-<h2>${arr.name}</h2>  
-<h2><i class="role"></i> ${arr.role}</h2>
-<hr>
-</div>
-<div class="card-body">
-<ul>
+    return `
+    <div class="col-md-4 mt-5">
+    <div class="card">
+    <div class="card-header">
+    <h2>${arr.name}</h2>  
+    <h2><i class="role"><img class="card-img-top" src="./imgs/Geek_Male_Circle_Blue.png"/></i> ${arr.role}</h2>
+    <hr>
+    </div>
+    <div class="card-body">
+    <div class="card-text">
+    <ul>
     <li>ID: ${arr.id}</li>
     <li>Email: ${arr.email}</li>
     <li>${roleInfo} </li>
-</ul>
-</div>
-</div>`
+    </ul>
+    </div>
+    </div>
+    </div>
+    </div>`
 }
 
-exports.generateHTML = generateHTML
+exports.generateHTML = generateHTML;
 exports.generateCard = generateCard;
